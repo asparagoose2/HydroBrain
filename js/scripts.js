6d080c2954f5039768a25119986ac20b1cd47494
ofir <ofir.duchovne@gmail.com>
2021-04-27 20:45:22 +0300

    search bar init

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,19 @@
-
 var changeFontSize = function () {
     console.log("here");    
     $($(".cell")).css('font-size', (($(".cell").width())*0.4));
 }
+
+var clickClick = function() {
+    if ($(this).css("background-image") != "none")
+        $(this).css("background-image","none");
+    else
+        $(this).css("background-image",'url("../images/beet.svg")');
+
+}
+
+$(".cell").each( function() {
+    this.onclick = clickClick
+    },);
 
 changeFontSize();
 window.onresize = changeFontSize
