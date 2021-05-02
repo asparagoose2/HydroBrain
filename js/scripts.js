0d83acbd2b2d007b25543f6cada2ed04de4f5e31
asparagoose2 <ofir.duchovne@gmail.com>
2021-05-02 20:59:06 +0300

    done - starting server tests

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,50 @@
-// var objects = [
-//     {"cell":10,"type":"beet"},
-//     {"cell":20,"type":"brocoli"},
-//     {"cell":40,"type":"beet"},
-//     {"cell":25,"type":"beet"},
-//     {"cell":32,"type":"cauliflower"}
-// ];
+var objects = [
+    {"cell":7,"type":"beet"},
+    {"cell":10,"type":"brocoli"},
+    {"cell":14,"type":"cauliflower"},
+    {"cell":19,"type":"lettuce"},
+    {"cell":21,"type":"eggplant"},
+    {"cell":28,"type":"lettuce"},
+    {"cell":48,"type":"spinach"},
+    {"cell":27,"type":"strawberry"},
+    {"cell":35,"type":"tomato"},
+    {"cell":22,"type":"brocoli"},
+    {"cell":23,"type":"beet"},
+    {"cell":24,"type":"cabage"},
+    {"cell":41,"type":"cauliflower"}
+];
 
-var objects = [];
+var moveObject = function(obj,x,y) {
+    $(obj).css("transform","translate("+x+","+y+")");
+}
 
+var sideBarBtnToggle = function() {
+    if($(".sideContent").css("visibility") == "hidden") {
+        $(".sideContent").css("visibility","visible");
+        moveObject($("#sideBarBtn"),"33vw","0%");
+        if ($("#xBtn").width()) {
+            $("#xBtn").attr("src","images/xBlack.svg");
+        } else {
+            $('img',"#sideBarBtn").attr("src","images/x.svg");
+        }
+    } else {
+        $(".sideContent").css("visibility","hidden");
+        moveObject($("#sideBarBtn"),"0%","0%");
+        $('img',"#sideBarBtn").attr("src","images/rightChevron.svg");
+    }
+}
 
 var isOverflown = function (element) {
     return element.scrollHeight > element.clientHeight || element.scrollWidth > element.clientWidth;
 }
 
 var resizeScreen = function() {
-    // while(isOverflown(document.body))
     while(document.body.scrollHeight > document.body.clientHeight)
     {
         $('body').css('width', $('body').width()-1);
     }
 }
 
-for (let index = 0; index < 50; index++) {
-    objects[index] = {"cell":index+1,"type":"brocoli"};   
-}
-
-
-// var renderMap = function() {
-//     objects.forEach((item) => {
-//         var index = 0;
-//         if(item["cell"]%10) {
-//             index = (Math.floor(item["cell"]/10))*10 + (10-item["cell"]%10);
-//         } else {
-//             index = (Math.floor(item["cell"]/10))*10 + (10-item["cell"]%10) -20;
-//         }
-//         console.log(index);
-//         $($($(".cell")[index]).children()[0]).attr("href", 'item.html?itemID=' + item["cell"]);
-//     });
-// }
 var renderMap = function() {
     objects.forEach((item) => {
         var index = 0;
@@ -46,40 +53,41 @@ var renderMap = function() {
         } else {
             index = (Math.floor(item["cell"]/10))*10 + (10-item["cell"]%10) -20;
         }
-        console.log(index);
         $($(".cell")[index]).css("background-image",'url("../images/'+item["type"]+'.svg'+'")');
     });
 }
 
-renderMap();
-
 var changeFontSize = function () {
-    console.log("here");    
     $($(".cell")).css('font-size', (($(".cell").width())*0.4));
 }
 
-var clickClick = function() {
-    if ($(this).css("background-image") != "none")
-        $(this).css("background-image","none");
-    else
-        $(this).css("background-image",'url("../images/beet.svg")');
-
-}
-
-$(".cell").each( function() {
-    this.onclick = clickClick
-    },);
-    
-changeFontSize();
-window.onresize = function() {
-    changeFontSize();
-    if(isOverflown(document.body)){
-        // resizeScreen();
+var openNav = function() {
+    document.getElementById("mySidenav").style.width = "20em";
+    if($(".sideContent").css("visibility","visible")) {
+        sideBarBtnToggle();
     }
 }
 
-// $($('.sort')[0]).css({
-//     style: 'btn-primary',
-//     showIcon: true,
-//     title: 'select test'
-// });
+var closeNav = function() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+$("#sideBarBtn").on("click", sideBarBtnToggle);
+
+$("body").on("click", closeNav);
+$(".burgerMenu").click(function(e) {
+    e.stopPropagation();
+});
+
+window.onresize = function() {
+    changeFontSize();
+    if($(window).width() / parseFloat($("body").css("font-size")) <= 60 && $(".sideContent").css("visibility") == "hidden") {
+        sideBarBtnToggle();
+    }
+    if($(window).width() / parseFloat($("body").css("font-size")) >= 80 && $(".sideContent").css("visibility") == "hidden") {
+        sideBarBtnToggle();
+    }
+}
+
+changeFontSize();
+renderMap();
